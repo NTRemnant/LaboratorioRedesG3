959c56b2e4f47b448c1aa2d27f7d1a2a30a6c642
Alberto <alberto.pizarro@usach.cl>
2019-01-06 19:46:33 -0300

    Conversiones de array
    Se añadieron funciones para pasar de imagen a array y viceversa (fases inicial y final del programa)

--- Futura entrega/BMP to WAV-ordenado.py
+++ Futura entrega/BMP to WAV-ordenado.py
@@ -4,7 +4,44 @@ import array
 import sys
 import struct
 
-def convertirWavBin(wav):
+def convertirBMptoArray(imagen):
+    bmp = open(imagen, 'rb')
+    raw = array.array('B')
+    i = 0
+
+    while i < 122:
+        extra = struct.unpack('B', bmp.read(1))
+        # print(extra[0])
+
+        if i == 18:
+            raw.append(extra[0])
+        elif i == 19:
+            raw.append(extra[0])
+        elif i == 20:
+            raw.append(extra[0])
+        elif i == 21:
+            raw.append(extra[0])
+        elif i == 22:
+            raw.append(extra[0])
+        elif i == 23:
+            raw.append(extra[0])
+        elif i == 24:
+            raw.append(extra[0])
+        elif i == 25:
+            raw.append(extra[0])
+
+        
+        i += 1
+    bmp.close()
+
+    img = Image.open(imagen)
+    for r, g, b in img.getdata():
+        raw.append(r)
+        raw.append(g)
+        raw.append(b)
+        i += 1
+
+    return raw
     
 
 def convertirBMPWAV(imagen, audio):
--- Futura entrega/WAV to BMP-ordenado.py
+++ Futura entrega/WAV to BMP-ordenado.py
@@ -8,7 +8,59 @@ import sys
 import math
 import binascii
 
+def convertirArraytoBMP(arreglo):
+    ancho=arreglo[0]*arreglo[1]*arreglo[2]*arreglo[3]
+    alto=arreglo[4]*arreglo[5]*arreglo[6]*arreglo[7]
+    #c=0
+    i=7
+    full_image = np.array([[[]]], dtype=int)        
+    c_linea = 0
+    c_pixel = 0
+    flag = 0
 
+    linea = np.array([[[]]], dtype=int)
+    pixel = np.array([[[]]], dtype=int)
+
+    for i in range(7, len(arreglo)):
+        extra = arreglo(i)
+        # print(extra)
+        if c_pixel == 0:
+            # print(extra , i)
+            pixel = np.array([[[extra]]])
+            c_pixel += 1
+        elif c_pixel == 1:
+            pixel = np.append(pixel, [[[extra]]], 2)  # append en 2: dentro del array o cuando es el primer valor, append en 1: al lado del array
+            c_pixel += 1
+        elif c_pixel == 2:
+            pixel = np.append(pixel, [[[extra]]], 2)
+            c_pixel = 0
+            # pixel completo, añado a la horizontal
+            if c_linea == 0:  # primer pixel en línea
+                linea = pixel
+            else:  # píxeles después del primero van en paralelo a este
+                linea = np.append(linea, pixel, 0)
+            pixel = np.array([[]], dtype=int)
+            c_linea += 1
+            if c_linea == ancho:  # linea completada
+                c_linea = 0
+                if flag == 0:
+                    full_image = linea  # np.append(full_image, linea, 1)
+                    flag = 1
+                else:
+                    full_image = np.append(full_image, linea, 1)
+                linea = np.array([[]], dtype=int)
+
+    
+    full_image = full_image.astype('uint8'))
+    print('Size total:', len(full_image))
+
+    print(full_image.dtype)
+    imagen_final = Image.fromarray(full_image, 'RGB')
+
+    imagen_final = imagen_final.transpose(Image.ROTATE_270)
+    imagen_final = imagen_final.transpose(Image.FLIP_LEFT_RIGHT)
+    imagen_final.save('output.BMP')
+    return 0
 
 def convertirWAVBMP(audio):
 
